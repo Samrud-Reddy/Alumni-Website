f44553a53d557724d84b7e4ddcaddd9f31be1e94
Samrud Reddy <samrudr@gmail.com>
2023-05-09 23:37:36 +0530

    fixed bug

--- public/routes/home.js
+++ public/routes/home.js
@@ -6,10 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var router = express_1.default.Router();
 var jwt_funcs_js_1 = require("../helper/jwt_funcs.js");
-var jwt = 6;
 router.get("/", function (req, res) {
     var send = { title: "My EJS App", message: "Hello, World!" };
-    var JWT_of_user = req.cookies.JWT;
+    var JWT_of_user = req.cookies.JWT_from_ggl;
     (0, jwt_funcs_js_1.verify_google_JWT)(JWT_of_user).then(function (data) {
         if (data) {
             res.render("index", send);
--- source/routes/home.ts
+++ source/routes/home.ts
@@ -3,10 +3,9 @@ const router = express.Router();
 
 import {verify_google_JWT} from "../helper/jwt_funcs.js";
 
-const jwt = 6;
 router.get("/", (req: Request, res: Response) => {
 	const send = {title: "My EJS App", message: "Hello, World!"};
-	const JWT_of_user = req.cookies.JWT;
+	const JWT_of_user = req.cookies.JWT_from_ggl;
 	verify_google_JWT(JWT_of_user).then((data: boolean) => {
 		if (data) {
 			res.render("index", send);
